317c93586a5adcef8a9e3fe0ab826d12ca283813
SeungGyu <phillipskl123@gmail.com>
2025-01-08 20:10:34 +0900

    [add] 개발환경 셋팅

---
+++ src/main/java/com/pickyfy/pickyfy/PickyfyApplication.java
@@ -0,0 +1,13 @@
+package com.pickyfy.pickyfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickyfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickyfyApplication.class, args);
+	}
+
+}
