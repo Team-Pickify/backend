7cf2019e8f59991f1448251f74aae44cf7a46331
sese2204 <sese2204@naver.com>
2025-01-10 18:09:54 +0900

    refactor: PickyfyApplication 클래스에 @EnableJpaAuditing 추가

--- src/main/java/com/pickyfy/pickyfy/PickyfyApplication.java
+++ src/main/java/com/pickyfy/pickyfy/PickyfyApplication.java
@@ -2,8 +2,10 @@ package com.pickyfy.pickyfy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PickyfyApplication {
 
 	public static void main(String[] args) {
