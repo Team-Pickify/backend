11ad8f7367d564d26fccd56ab7ed6b68a1f2fa38
양효빈 <hyobin3026@naver.com>
2025-01-11 18:03:35 +0900

    [feat] #5 응답 반환 로직 추상화

---
+++ src/main/java/com/pickyfy/pickyfy/apiPayload/code/BaseCode.java
@@ -0,0 +1,8 @@
+package com.pickyfy.pickyfy.apiPayload.code;
+
+import com.pickyfy.pickyfy.apiPayload.code.dto.ReasonDTO;
+
+public interface BaseCode {
+    ReasonDTO getReason();
+    ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/com/pickyfy/pickyfy/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,8 @@
+package com.pickyfy.pickyfy.apiPayload.code;
+
+import com.pickyfy.pickyfy.apiPayload.code.dto.ErrorReasonDTO;
+
+public interface BaseErrorCode {
+    ErrorReasonDTO getReason();
+    ErrorReasonDTO getReasonHttpStatus();
+}
