c83a948825699e98c02e32877fd63f91fb8972bb
양효빈 <hyobin3026@naver.com>
2025-01-12 18:46:22 +0900

    [feat] #5 status code를 enum 클래스로 상수화

---
+++ src/main/java/com/pickyfy/pickyfy/apiPayload/code/status/StatusCode.java
@@ -0,0 +1,22 @@
+package com.pickyfy.pickyfy.apiPayload.code.status;
+
+public enum StatusCode {
+
+    COMMON("COMMON"),
+    USER("USER"),
+    PLACE("PLACE"),
+    MAGAZINE("MAGAZINE"),
+    ADMIN("ADMIN"),
+    CATEGORY("CATEGORY"),
+    IMAGE("IMAGE");
+
+    private final String prefix;
+
+    StatusCode(String prefix){
+        this.prefix = prefix;
+    }
+
+    public String getCode(int codeNumber){
+        return this.prefix + codeNumber;
+    }
+}
