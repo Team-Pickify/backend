2e907317aab9b64851e701c9f2f8f5990b5f6001
양효빈 <hyobin3026@naver.com>
2025-01-20 22:08:40 +0900

    [feature] #21 Admin 클래스 구현

---
+++ src/main/java/com/pickyfy/pickyfy/domain/Admin.java
@@ -0,0 +1,29 @@
+package com.pickyfy.pickyfy.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public Admin(String name, String password){
+        this.name = name;
+        this.password = password;
+    }
+}
