0784fe470cfd9129b3a1f0be3f5c165c8b8d56cd
양효빈 <hyobin3026@naver.com>
2025-01-20 22:13:37 +0900

    [feature] #21 AdminRepository 구현

---
+++ src/main/java/com/pickyfy/pickyfy/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.pickyfy.pickyfy.repository;
+
+import com.pickyfy.pickyfy.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String name);
+}
