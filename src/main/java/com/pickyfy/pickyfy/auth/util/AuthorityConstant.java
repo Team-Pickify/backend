13e1be922fd59c49f9c8c54145ca8e8ce7821ec3
양효빈 <hyobin3026@naver.com>
2025-01-21 12:01:40 +0900

    [feature] #21 권한 관련 상수 클래스 구현

---
+++ src/main/java/com/pickyfy/pickyfy/auth/util/AuthorityConstant.java
@@ -0,0 +1,8 @@
+package com.pickyfy.pickyfy.auth.util;
+
+public class AuthorityConstant {
+
+    public static final String ACCESS_TOKEN_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final int BEARER_LENGTH = 7;
+}
