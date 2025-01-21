ee45850e3af3489267e3aad820a992e2f91c5e33
양효빈 <hyobin3026@naver.com>
2025-01-21 12:02:33 +0900

    [feature] #21 jwt 필터 인터페이스 구현

--- src/main/java/com/pickyfy/pickyfy/auth/filter/JwtAuthFilter.java
+++ src/main/java/com/pickyfy/pickyfy/auth/filter/JwtAuthFilter.java
@@ -1,55 +1,16 @@
 package com.pickyfy.pickyfy.auth.filter;
 
-import com.pickyfy.pickyfy.auth.util.JwtUtil;
-import com.pickyfy.pickyfy.auth.custom.CustomUserDetails;
-import com.pickyfy.pickyfy.auth.custom.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
-public class JwtAuthFilter extends OncePerRequestFilter {
+public interface JwtAuthFilter {
 
-    private static final String ACCESS_TOKEN_HEADER = "Authorization";
-    private static final String BEARER = "Bearer ";
+    void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException;
 
-    private final CustomUserDetailsService customUserDetailsService;
-    private final JwtUtil jwtUtil;
+    String resolveToken(HttpServletRequest request);
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-
-        String authorizationHeader = request.getHeader(ACCESS_TOKEN_HEADER);
-
-        if (authorizationHeader == null || !authorizationHeader.startsWith(BEARER)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        String token = authorizationHeader.substring(7);
-
-        if (!jwtUtil.validateToken(token)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        String email = jwtUtil.getUserEmail(token);
-        CustomUserDetails userDetails = (CustomUserDetails) customUserDetailsService.loadUserByEmail(email);
-
-        if (userDetails == null) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-
-        filterChain.doFilter(request, response);
-    }
 }
