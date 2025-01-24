b17d47a63d0044e09843b38a9c326ff8f8ab11ee
sese2204 <sese2204@naver.com>
2025-01-24 15:57:43 +0900

    refactor: CategoryService 추상화 계층 추가

--- src/main/java/com/pickyfy/pickyfy/service/CategoryService.java
+++ src/main/java/com/pickyfy/pickyfy/service/CategoryService.java
@@ -1,37 +1,15 @@
 package com.pickyfy.pickyfy.service;
 
 import com.pickyfy.pickyfy.domain.Category;
-import com.pickyfy.pickyfy.repository.CategoryRepository;
 import com.pickyfy.pickyfy.web.dto.request.CategoryCreateRequest;
-import jakarta.validation.constraints.NotBlank;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.pickyfy.pickyfy.web.dto.request.CategoryUpdateRequest;
+import com.pickyfy.pickyfy.web.dto.response.CategoryResponse;
+import java.util.List;
 
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class CategoryService {
-
-    private final CategoryRepository categoryRepository;
-
-    @Transactional
-    public Long createCategory(CategoryCreateRequest categoryCreateRequest) {
-        validateDuplicateName(categoryCreateRequest.name());
-
-        Category category = Category.builder()
-                .name(categoryCreateRequest.name())
-                .build();
-
-        return categoryRepository.save(category).getId();
-    }
-
-    private void validateDuplicateName(String name) {
-        if (categoryRepository.existsByName(name)) {
-            /*
-            TODO: Custom Exception 만들어서 던지기
-            throw new DuplicateResourceException("Category already exists with name: " + name);
-             */
-        }
-    }
+public interface CategoryService {
+    public Long createCategory(CategoryCreateRequest categoryCreateRequest);
+    public CategoryResponse getCategory(Long id);
+    public List<CategoryResponse> getAllCategories();
+    public void updateCategory(Long id, CategoryUpdateRequest request);
+    public void deleteCategory(Long id);
 }
