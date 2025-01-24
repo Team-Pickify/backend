044b82e1090d7418c6604aed1b4f376c0ed6c264
sese2204 <sese2204@naver.com>
2025-01-24 16:03:20 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/com/pickyfy/pickyfy/service/CategoryService.java
+++ src/main/java/com/pickyfy/pickyfy/service/CategoryService.java
@@ -1,15 +1,14 @@
 package com.pickyfy.pickyfy.service;
 
-import com.pickyfy.pickyfy.domain.Category;
 import com.pickyfy.pickyfy.web.dto.request.CategoryCreateRequest;
 import com.pickyfy.pickyfy.web.dto.request.CategoryUpdateRequest;
 import com.pickyfy.pickyfy.web.dto.response.CategoryResponse;
 import java.util.List;
 
 public interface CategoryService {
-    public Long createCategory(CategoryCreateRequest categoryCreateRequest);
-    public CategoryResponse getCategory(Long id);
-    public List<CategoryResponse> getAllCategories();
-    public void updateCategory(Long id, CategoryUpdateRequest request);
-    public void deleteCategory(Long id);
+    Long createCategory(CategoryCreateRequest categoryCreateRequest);
+    CategoryResponse getCategory(Long id);
+    List<CategoryResponse> getAllCategories();
+    void updateCategory(Long id, CategoryUpdateRequest request);
+    void deleteCategory(Long id);
 }
