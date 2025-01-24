ec03085a9269ca64a8744c607246c6662e61e574
sese2204 <sese2204@naver.com>
2025-01-24 16:03:32 +0900

    refactor: 사용하지 않는 메소드 제거

--- src/main/java/com/pickyfy/pickyfy/repository/CategoryRepository.java
+++ src/main/java/com/pickyfy/pickyfy/repository/CategoryRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    Optional<Category> findByName(String name);
     Optional<Category> findById(Long id);
     boolean existsByName(String name);
 }
