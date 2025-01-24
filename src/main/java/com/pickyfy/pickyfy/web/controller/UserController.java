3780b062a9a369bfc2b54f19d3553a7bbbb0f41a
SeungGyu <phillipskl123@gmail.com>
2025-01-24 22:21:15 +0900

    [fix] #26 OAuth 통합을 위해 password nullable로 변경

--- src/main/java/com/pickyfy/pickyfy/web/controller/UserController.java
+++ src/main/java/com/pickyfy/pickyfy/web/controller/UserController.java
@@ -4,6 +4,7 @@ import com.pickyfy.pickyfy.apiPayload.ApiResponse;
 import com.pickyfy.pickyfy.web.dto.request.UserCreateRequest;
 import com.pickyfy.pickyfy.web.dto.response.UserCreateResponse;
 import com.pickyfy.pickyfy.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class UserController implements UserControllerApi{
     private final UserService userService;
 
     @Override
-    public ApiResponse<UserCreateResponse> signUp(UserCreateRequest userCreateRequest) {
+    public ApiResponse<UserCreateResponse> signUp(@Valid UserCreateRequest userCreateRequest) {
         UserCreateResponse userCreateResponse = userService.signUp(userCreateRequest);
         return ApiResponse.onSuccess(userCreateResponse);
     }
