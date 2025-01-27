97894e002ab1b1c336359714ae74d1dd385ee099
sese2204 <sese2204@naver.com>
2025-01-27 16:34:09 +0900

    feat: MagazineRepository 인터페이스 구현

---
+++ src/main/java/com/pickyfy/pickyfy/repository/MagazineRepository.java
@@ -0,0 +1,7 @@
+package com.pickyfy.pickyfy.repository;
+
+import com.pickyfy.pickyfy.domain.Magazine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MagazineRepository extends JpaRepository<Magazine, Long> {
+}
