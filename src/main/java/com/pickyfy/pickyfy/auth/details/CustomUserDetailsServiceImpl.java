ee4bddfffcd7d2c31b8e67a30b9baf37f8bcd195
SeungGyu <phillipskl123@gmail.com>
2025-01-28 18:50:03 +0900

    [feat] 일반 로그인 API 구현

---
+++ src/main/java/com/pickyfy/pickyfy/auth/details/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package com.pickyfy.pickyfy.auth.details;
+
+import com.pickyfy.pickyfy.domain.Admin;
+import com.pickyfy.pickyfy.domain.User;
+import com.pickyfy.pickyfy.repository.AdminRepository;
+import com.pickyfy.pickyfy.web.dto.CustomAdminInfoDto;
+import com.pickyfy.pickyfy.web.dto.CustomUserInfoDto;
+import com.pickyfy.pickyfy.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
+
+    private final static String NOT_FOUND_USER = "해당하는 유저가 없습니다.";
+
+    private final UserRepository userRepository;
+    private final AdminRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByName(nickname)
+                .orElseThrow(() -> new UsernameNotFoundException(NOT_FOUND_USER));
+        CustomAdminInfoDto customAdminInfoDto = CustomAdminInfoDto.from(admin);
+
+        return new CustomAdminDetails(customAdminInfoDto);
+    }
+
+    public UserDetails loadUserByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(NOT_FOUND_USER));
+        CustomUserInfoDto customUserInfoDto = CustomUserInfoDto.from(user);
+
+        return new CustomUserDetails(customUserInfoDto);
+    }
+}
---
+++ src/main/java/com/pickyfy/pickyfy/auth/filter/CustomLoginFilter.java
@@ -0,0 +1,83 @@
+package com.pickyfy.pickyfy.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pickyfy.pickyfy.common.util.JwtUtil;
+import com.pickyfy.pickyfy.common.util.RedisUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private static final String CAN_NOT_READ_BODY = "인증 요청 본문을 읽을 수 없습니다.";
+    private static final long REFRESH_TOKEN_EXPIRATION_TIME = 7 * 24 * 60 * 60;
+
+    private final AuthenticationManager authenticationManager;
+    private final ObjectMapper objectMapper;
+    private final JwtUtil jwtUtil;
+    private final RedisUtil redisUtil;
+
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            Map<String, String> credentials = objectMapper.readValue(request.getInputStream(), Map.class);
+            String username = credentials.get("principal");
+            String password = credentials.get("password");
+
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+
+            return authenticationManager.authenticate(token);
+        } catch (IOException e) {
+            throw new AuthenticationServiceException(CAN_NOT_READ_BODY);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication){
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String principal = userDetails.getUsername();
+        String role = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElse(null);
+
+        String accessToken = jwtUtil.createAccessToken(principal, role);
+        String refreshToken = jwtUtil.createRefreshToken(principal, role);
+
+        redisUtil.setDataExpire("refresh:" + jwtUtil.getPrincipal(refreshToken), refreshToken, REFRESH_TOKEN_EXPIRATION_TIME);
+        response.setHeader("Authorization", "Bearer " + accessToken);
+        ResponseCookie cookie = createCookie("refreshToken", refreshToken, REFRESH_TOKEN_EXPIRATION_TIME);
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        getFailureHandler().onAuthenticationFailure(request, response, exception);
+    }
+
+    public ResponseCookie createCookie(String name, String value, long maxAge) {
+        return ResponseCookie.from(name, value)
+                .secure(true)
+                .sameSite("None")
+                .httpOnly(true)
+                .path("/")
+                .maxAge(maxAge)
+                .build();
+    }
+}
