9fae5eaad2f785436ce9b39489d0b4659c0f0bec
SeungGyu <phillipskl123@gmail.com>
2025-01-28 18:45:49 +0900

    [feat] 카카오 소셜 로그인 API 구현

---
+++ src/main/java/com/pickyfy/pickyfy/auth/handler/OAuth2FailureHandler.java
@@ -0,0 +1,25 @@
+package com.pickyfy.pickyfy.auth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws ServletException, IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String jsonResponse = String.format("{\"error\": \"Authentication failed\", \"message\": \"%s\"}", authenticationException.getMessage());
+        response.getWriter().write(jsonResponse);
+    }
+}
--- src/main/java/com/pickyfy/pickyfy/config/SecurityConfig.java
+++ src/main/java/com/pickyfy/pickyfy/config/SecurityConfig.java
@@ -1,17 +1,27 @@
 package com.pickyfy.pickyfy.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pickyfy.pickyfy.auth.filter.CustomLoginFilter;
+import com.pickyfy.pickyfy.auth.handler.CustomAuthenticationFailureHandler;
+import com.pickyfy.pickyfy.auth.oauth2.CustomAuthorizationRequestResolver;
+import com.pickyfy.pickyfy.auth.handler.OAuth2FailureHandler;
+import com.pickyfy.pickyfy.auth.handler.OAuth2SuccessHandler;
 import com.pickyfy.pickyfy.auth.filter.JwtAuthFilter;
 import com.pickyfy.pickyfy.common.util.JwtUtil;
-import com.pickyfy.pickyfy.auth.custom.CustomUserDetailsService;
+import com.pickyfy.pickyfy.auth.details.CustomUserDetailsServiceImpl;
+import com.pickyfy.pickyfy.common.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,12 +30,35 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomUserDetailsService customUserDetailsService;
+    private final AuthenticationConfiguration configuration;
+    private final CustomAuthorizationRequestResolver customAuthorizationRequestResolver;
+    private final DefaultOAuth2UserService oAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+    private final ObjectMapper objectMapper;
+    private final CustomUserDetailsServiceImpl customUserDetailsServiceImpl;
     private final JwtUtil jwtUtil;
+    private final RedisUtil redisUtil;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
 
+        http
+                .securityMatcher("/auth/oauth2/**", "/oauth2/callback/**")
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/auth/oauth2/**", "/oauth2/callback/**", "/**").permitAll())
+                .oauth2Login(oauth2 -> oauth2
+                        .authorizationEndpoint(endpoint -> endpoint
+                                .authorizationRequestResolver(customAuthorizationRequestResolver))
+                        .redirectionEndpoint(url -> url.baseUri("/oauth2/callback"))
+                        .userInfoEndpoint(endpoint -> endpoint.userService(oAuth2UserService))
+                        .successHandler(oAuth2SuccessHandler)
+                        .failureHandler(oAuth2FailureHandler));
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
@@ -34,18 +67,38 @@ public class SecurityConfig {
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http
+                .securityMatcher("/**")
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/users/signup", "/auth/login/**", "/admin", "/email-auth/**").permitAll()
-                        .requestMatchers( "/","/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/users/signup", "/auth/login", "/email-auth/**", "auth/oauth2/**").permitAll()
+                        .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated());
 
         http
-                .addFilterBefore(new JwtAuthFilter(customUserDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtAuthFilter(customUserDetailsServiceImpl, jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(customLoginFilter(authenticationManager((configuration))), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    public CustomLoginFilter customLoginFilter(AuthenticationManager authenticationManager) {
+        CustomLoginFilter customLoginFilter = new CustomLoginFilter(
+                authenticationManager,
+                objectMapper,
+                jwtUtil,
+                redisUtil
+        );
+        customLoginFilter.setFilterProcessesUrl("/auth/login");
+        customLoginFilter.setAuthenticationFailureHandler(new CustomAuthenticationFailureHandler());
+        return customLoginFilter;
     }
 }
