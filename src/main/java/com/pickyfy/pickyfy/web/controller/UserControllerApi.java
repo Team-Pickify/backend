107fec20191ccfde2e7a740e11424b0cbe09aad8
SeungGyu <phillipskl123@gmail.com>
2025-01-28 18:46:54 +0900

    [add] UserController @Tag 추가

--- src/main/java/com/pickyfy/pickyfy/web/controller/UserControllerApi.java
+++ src/main/java/com/pickyfy/pickyfy/web/controller/UserControllerApi.java
@@ -5,9 +5,11 @@ import com.pickyfy.pickyfy.web.dto.request.UserCreateRequest;
 import com.pickyfy.pickyfy.web.dto.response.UserCreateResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Tag(name = "사용자")
 public interface UserControllerApi {
 
     @Operation(summary = "회원가입 API", description = "회원가입 API입니다. 닉네임, 비밀번호, 이메일,이메일 토큰을 입력해주세요 ")
