ab385989992f413b27326eaaf084dd728b942071
양효빈 <hyobin3026@naver.com>
2025-01-29 16:09:56 +0900

    [feature]: 관리자 컨트롤러 구현

--- src/main/java/com/pickyfy/pickyfy/web/controller/AdminController.java
+++ src/main/java/com/pickyfy/pickyfy/web/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
-public class AdminController {
+public class AdminController implements AdminControllerApi{
 
     private final AdminService adminService;
 
