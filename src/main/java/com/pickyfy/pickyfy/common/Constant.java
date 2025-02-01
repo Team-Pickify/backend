afc81d317d14ad89bbaeed2dfe11b54dcb49c7ec
양효빈 <hyobin3026@naver.com>
2025-02-01 23:02:32 +0900

    [refactor]: 비밀번호 정규식 수정

--- src/main/java/com/pickyfy/pickyfy/common/Constant.java
+++ src/main/java/com/pickyfy/pickyfy/common/Constant.java
@@ -2,7 +2,7 @@ package com.pickyfy.pickyfy.common;
 
 public class Constant {
 
-    public static final String PASSWORD_REGX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/-]).{8,}$";
+    public static final String PASSWORD_REGX = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/-]).{8,}$";
     public static final String NICKNAME_REGX = "^[a-zA-Z가-힣0-9!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/-]{1,8}$";
 
     public static final String REDIS_KEY_PREFIX = "refresh:";
