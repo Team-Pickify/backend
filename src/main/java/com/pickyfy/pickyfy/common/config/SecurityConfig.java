951855d7332ce0c28a67eb990c46e2365bcf72c7
양효빈 <hyobin3026@naver.com>
2025-02-07 11:48:08 +0900

    [fix]: 이메일 인증 에러 수정

--- src/main/java/com/pickyfy/pickyfy/common/config/SecurityConfig.java
+++ src/main/java/com/pickyfy/pickyfy/common/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
         http
                 .securityMatcher("/**")
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/users/signup", "/auth/login", "/email-auth/**", "auth/oauth2/**").permitAll()
+                        .requestMatchers("/users/signup", "/auth/login", "/email-auth/**", "auth/oauth2/**", "/users/verify-by-email").permitAll()
                         .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated());
