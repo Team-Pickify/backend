826419c23b24ea7ae7241b2b0f68c32892b79716
양효빈 <hyobin3026@naver.com>
2025-02-16 16:12:50 +0900

    [test] 커스텀 애노테이션 테스트 코드 추가

--- src/test/java/com/pickyfy/pickyfy/common/util/AllFieldsNotNullValidatorTest.java
+++ src/test/java/com/pickyfy/pickyfy/common/util/AllFieldsNotNullValidatorTest.java
@@ -8,6 +8,7 @@ import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,31 +24,31 @@ class AllFieldsNotNullValidatorTest {
     }
 
     @AllFieldsNotNull
-    record TestClass (String name, Integer age){}
+    record TestClass (String name, Integer age, List<Integer> ids){}
 
     @Test
-    void 모든_필드가_null이_아닐_때_성공(){
-        TestClass validObject = new TestClass("피키파이", 20);
+    void succeed_when_all_fields_are_not_null(){
+        TestClass validObject = new TestClass("피키파이", 20, List.of(1,2,3));
         Set<ConstraintViolation<TestClass>> violations = validator.validate(validObject);
         assertTrue(violations.isEmpty());
     }
 
     @Test
-    void 하나의_필드가_null이면_실패() {
-        TestClass invalidObject = new TestClass(null, 30);
+    void fail_when_some_fields_are_null() {
+        TestClass invalidObject = new TestClass(null, 30, null);
         Set<ConstraintViolation<TestClass>> violations = validator.validate(invalidObject);
         assertFalse(violations.isEmpty());
     }
 
     @Test
-    void 모든_필드가_null이면_실패() {
-        TestClass invalidObject = new TestClass(null, null);
+    void fail_when_all_fields_are_null() {
+        TestClass invalidObject = new TestClass(null, null, null);
         Set<ConstraintViolation<TestClass>> violations = validator.validate(invalidObject);
         assertFalse(violations.isEmpty());
     }
 
     @Test
-    void 객체가_null이면_실패() {
+    void fail_when_object_is_null() {
         TestClass nullObject = null;
         assertThrows(IllegalArgumentException.class, () -> validator.validate(nullObject));
     }
