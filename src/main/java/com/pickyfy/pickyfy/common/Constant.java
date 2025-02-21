88fa5e4fcbf905b20423523426048a229035ee6b
SeungGyu <phillipskl123@gmail.com>
2025-02-21 13:14:52 +0900

    [refactor] 엑세스토큰 유효기간 변경

--- src/main/java/com/pickyfy/pickyfy/common/Constant.java
+++ src/main/java/com/pickyfy/pickyfy/common/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     public static final String NICKNAME_REGX = "^[a-zA-Z가-힣0-9!@#$%^&*()_+={}\\[\\]:;\"'<>,.?/-]{1,8}$";
 
     public static final long EMAIL_TOKEN_EXPIRATION_TIME = 5 * 60 * 1000;
-    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 30 * 60 * 1000;
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 10 * 30 * 60 * 1000;
     public static final long REFRESH_TOKEN_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000;
 
     public static final long CATEGORIES_EXPIRATION_TIME = 24 * 60 * 60 * 1000;
